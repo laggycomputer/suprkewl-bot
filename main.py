a568d9daebf1ed56281fdd83bf83adfab0ecee62
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2018-12-07 22:58:13 -0800

    Moved client functionality in main file into its own class

--- main.py
+++ main.py
@@ -10,83 +10,86 @@ import asyncio
 import random
 import platform
 
-startup_extensions = ["jishaku", "ext.Text", "ext.Randomizers", "ext.Documentation", "ext.Moderation", "ext.Info", "testing"]
+class theBot(commands.Bot):
 
-client = commands.Bot(command_prefix = "s!", description = "SuprKewl Bot, by Too Laggy#3878", pm_help = True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-async def playingstatus():
+        self.bg_task = self.loop.create_task(self.playingstatus())
 
-    await client.wait_until_ready()
-    
-    playing_statuses = ["with the community",
-                        "with my dad, Too Laggy",
-                        "github.com/laggycomputer/suprkewl-bot",
-                        "being open source",
-                        "I don't game...",
-                        "waiting for you to call me! s!help",
-                        "being SuprKewl!",
-                        "with my Raspberry Pi",
-                        "creeping through the shadows",
-                        "eating robot food, brb",
-                        "being improved!",
-                        "ping and run",
-                        "helping the community",
-                        "living under the MIT license!",
-                        "at a robot party, brb in a bit",
-                        "meme-scrolling",
-                        "and plotting pranks",
-                        "with the Discord API"]
+        startup_extensions = ["jishaku", "ext.Text", "ext.Randomizers", "ext.Documentation", "ext.Moderation", "ext.Info", "testing"]
 
-    while not client.is_closed():
-        status = f"{random.choice(playing_statuses)} | lurking in {len(client.guilds)} servers and watching over {len(client.users)} users..."
+        for extension in startup_extensions:
+            try:
+                self.load_extension(extension)
+                print("Loaded module {}. yay".format(extension))
+            except Exception as e:
+                exc = f"{e.__name__}: {e}"
+                print(f"Failed to load extension {extension}\n{exc}")
 
-        await client.change_presence(activity = discord.Game(name = status))
-        await asyncio.sleep(120)
+    async def on_ready(self):
 
-client.bg_task = client.loop.create_task(playingstatus())
+        print(f"Logged in as {self.user.name} (ID {self.user.id} | Connected to {len(self.guilds)} servers | Connected to {len(set(client.get_all_members()))} users")
+        print("-" * 8)
+        print(f"Current Discord.py Version: {discord.__version__} | Current Python Version: {platform.python_version()}")
+        print("-" * 8)
+        print(f"Use this link to invite {self.user.name}:")
+        print(f"https://discordapp.com/oauth2/authorize?client_id={self.user.id}&scope=bot&permissions=8")
+        print("-" * 8)
 
-@client.event
-async def on_ready():
+    async def on_message(self, message):
 
-    print(f"Logged in as {client.user.name} (ID {client.user.id} | Connected to {len(client.guilds)} servers | Connected to {len(set(client.get_all_members()))} users")
-    print("-" * 8)
-    print(f"Current Discord.py Version: {discord.__version__} | Current Python Version: {platform.python_version()}")
-    print("-" * 8)
-    print(f"Use this link to invite {client.user.name}:")
-    print(f"https://discordapp.com/oauth2/authorize?client_id={client.user.id}&scope=bot&permissions=8")
-    print("-" * 8)
+        print(f"Got message '{message.content}'")
+        print(f"From @{message.author}")
+        print(f"In server {message.guild}")
+        print(f"In channel {message.channel}")
 
-@client.event
-async def on_message(message):
-
-    print(f"Got message '{message.content}'")
-    print(f"From @{message.author}")
-    print(f"In server {message.guild}")
-    print(f"In channel {message.channel}")
-
-    if message.author.bot:
-        return
-    else:
-        if isinstance(message.channel, discord.abc.GuildChannel):
-            if message.channel.permissions_for(message.guild.me).send_messages:
-
-                if message.content == "<@408869071946514452>":
-                    await message.channel.send(":eyes: **WHO DARE PING** btw my prefix is `s!`.")
-
-                await client.process_commands(message)
-
-            else:
-                if message.content.startswith("s!"):
-                    await message.author.send(":x: I can't send messages there! Perhaps try again elsewhere?")
+        if message.author.bot:
+            return
         else:
-            await client.process_commands(message)
+            if isinstance(message.channel, discord.abc.GuildChannel):
+                if message.channel.permissions_for(message.guild.me).send_messages:
 
-if __name__ == "__main__":
-    for extension in startup_extensions:
-        try:
-            client.load_extension(extension)
-            print("Loaded module {}. yay".format(extension))
-        except Exception as e:
-            exc = f"{e.__name__}: {e}"
-            print(f"Failed to load extension {extension}\n{exc}")
+                    if message.content == "<@408869071946514452>":
+                        await message.channel.send(":eyes: **WHO DARE PING** btw my prefix is `s!`.")
+
+                    await self.process_commands(message)
+
+                else:
+                    if message.content.startswith("s!"):
+                        await message.author.send(":x: I can't send messages there! Perhaps try again elsewhere?")
+            else:
+                await self.process_commands(message)
+
+    async def playingstatus(self):
+
+        await self.wait_until_ready()
+        
+        playing_statuses = ["with the community",
+                            "with my dad, Too Laggy",
+                            "github.com/laggycomputer/suprkewl-bot",
+                            "being open source",
+                            "I don't game...",
+                            "waiting for you to call me! s!help",
+                            "being SuprKewl!",
+                            "with my Raspberry Pi",
+                            "creeping through the shadows",
+                            "eating robot food, brb",
+                            "being improved!",
+                            "ping and run",
+                            "helping the community",
+                            "living under the MIT license!",
+                            "at a robot party, brb in a bit",
+                            "meme-scrolling",
+                            "and plotting pranks",
+                            "with the Discord API"]
+
+        while not self.is_closed():
+            status = f"{random.choice(playing_statuses)} | lurking in {len(self.guilds)} servers and watching over {len(self.users)} users..."
+
+            await self.change_presence(activity = discord.Game(name = status))
+            await asyncio.sleep(120)
+
+client = theBot(command_prefix = "s!", description = "SuprKewl Bot, by Too Laggy#3878", pm_help = True)
+
 client.run("TOKEN")
