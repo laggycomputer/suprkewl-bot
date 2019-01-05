80df1c70d6203e88a1db71e65bb6e58833014761
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2019-01-05 08:57:00 -0800

    Added global error handler for the failure of permission checks.

--- main.py
+++ main.py
@@ -100,16 +100,32 @@ class theBot(commands.Bot):
 
         error = getattr(error, "original", error)
 
-		def perms_list(perms):	
+		def permsList(perms):
+			apiToHuman = {"add_reactions": "Add Reactions", "administrator": "Administrator", "attach_files": "Attach Files",
+      					  "ban_members": "Ban Members", "change_nickname": "Can Change Own Nickname", "connect": "Connect to Voice Channels",
+ 						  "create_instant_invite": "Create Server Invites", "deafen_members": "Deafen Members", "embed_links": "Embed Links",
+ 						  "external_emojis": "(Nitro Only) Use External Emotes", "kick_members": "Kick Members", "manage_channels": "Change, Create, and Delete Roles",
+ 						  "manage_emojis": "Create, Delete, and Rename Server Emotes", "manage_guild": "Manage Server", "manage_messages": "Manage Messages",
+ 						  "manage_messages": "Manage Messages", "manage_nicknames": "Manage Nicknames", "manage_roles": "Manage Roles", "manage_webhooks": "Manage Webhooks",
+ 						  "mention_everyone": "Ping @\u200beveryone and @\u200bhere", "move_members": "Move Members Between Voice Channels", "mute_members": "Mute Members",
+ 						  "priority_speaker": "Use Priority PTT", "read_message_history": "Read Past Messages in Text Channels", "read_messages": "Read Messages and See Voice Channels",
+ 						  "send_messages": "Send Messages", "send_tts_messages": "Send TTS Messages", "speak": "Speak", "use_voice_activation": "No Voice Activity",
+ 						  "view_audit_log": "View the Server Audit Log"}
 			if len(perms) == 0:
 				return None
 			else:
 				if len(perms) == 1:
-					return len[0]
+					return apiToHuman[perms[0]]
 				else:
-					list = ", ".join(perms[0:-1])
-					list += f", and {perms[-1]}"
-					return list
+					fmt = ""
+					
+					for i in range(0, len(perms)):
+						fmt += apiToHuman[i]
+						fmt += ", "
+
+					fmt += f"and {perms[-1]}"
+					
+					return fmt
 		
         if isinstance(error, ignored):
             return
@@ -152,7 +168,7 @@ class theBot(commands.Bot):
 		elif isinstance(error, commands.MissingPermissions):
 		
 			emb = discord.Embed
-			missingPerms = perms_list(error.missing_perms)
+			missingPerms = permsList(error.missing_perms)
 			emb.add_field(name="User Missing Permissions", value=f":x: Permission denied to run {ctx.prefix}{ctx.command}. You need to be able to {missingPerms}.")
 			emb.set_footer(f"Command invoked by {ctx.author}")
 			
@@ -161,7 +177,7 @@ class theBot(commands.Bot):
 		elif isinstance(error, commands.BotMissingPermissions):
 			
 			emb = discord.Embed
-			missingPerms = perms_list(error.missing_perms)
+			missingPerms = permsList(error.missing_perms)
 			emb.add_field(name="Bot Missing Permissions", value=f":x: I don't have the proper permissions to run {ctx.prefix}{ctx.command}. I need to be allowed to {missingPerms}.")
 			emb.set_footer(f"Command invoked by {ctx.author}")
 			
