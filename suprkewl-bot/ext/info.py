b124db5aba3e158b7252b9578050f140cc6d624c
Dante Dam <laggycomputer@yahoo.com>
2019-02-28 19:36:17 -0800

    Small spacing fixes

--- suprkewl-bot/ext/info.py
+++ suprkewl-bot/ext/info.py
@@ -29,8 +29,9 @@ class Info(commands.Cog):
         emb.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
         emb.set_footer(text=f"{self.bot.description} Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
 
+        dispHoist = "No"
         if role.hoist:
-            dispHoist="Yes"
+            dispHoist = "Yes"
         emb.add_field(name="'Display role member seperately from online members'", value=dispHoist)
 
         await ctx.send(embed=emb)
@@ -52,13 +53,13 @@ class Info(commands.Cog):
         apiToHuman = {"add_reactions": "Add Reactions", "administrator": "Administrator", "attach_files": "Attach Files",
                       "ban_members": "Ban Members", "change_nickname": "Can Change Own Nickname", "connect": "Connect to Voice Channels",
                       "create_instant_invite": "Create Server Invites", "deafen_members": "Deafen Members", "embed_links": "Embed Links",
-					  "external_emojis": "(Nitro Only) Use External Emotes", "kick_members": "Kick Members", "manage_channels": "Change, Create, and Delete Roles",
-					  "manage_emojis": "Create, Delete, and Rename Server Emotes", "manage_guild": "Manage Server", "manage_messages": "Manage Messages",
-					  "manage_messages": "Manage Messages", "manage_nicknames": "Manage Nicknames", "manage_roles": "Manage Roles", "manage_webhooks": "Manage Webhooks",
-					  "mention_everyone": "Ping @\u200beveryone and @\u200bhere", "move_members": "Move Members Between Voice Channels", "mute_members": "Mute Members",
-					  "priority_speaker": "Use Priority PTT", "read_message_history": "Read Past Messages in Text Channels", "read_messages": "Read Messages and See Voice Channels",
-					  "send_messages": "Send Messages", "send_tts_messages": "Send TTS Messages", "speak": "Speak", "use_voice_activation": "No Voice Activity",
-					  "view_audit_log": "View the Server Audit Log"}
+		      "external_emojis": "(Nitro Only) Use External Emotes", "kick_members": "Kick Members", "manage_channels": "Change, Create, and Delete Roles",
+                      "manage_emojis": "Create, Delete, and Rename Server Emotes", "manage_guild": "Manage Server", "manage_messages": "Manage Messages",
+                      "manage_messages": "Manage Messages", "manage_nicknames": "Manage Nicknames", "manage_roles": "Manage Roles", "manage_webhooks": "Manage Webhooks",
+                      "mention_everyone": "Ping @\u200beveryone and @\u200bhere", "move_members": "Move Members Between Voice Channels", "mute_members": "Mute Members",
+                      "priority_speaker": "Use Priority PTT", "read_message_history": "Read Past Messages in Text Channels", "read_messages": "Read Messages and See Voice Channels",
+                      "send_messages": "Send Messages", "send_tts_messages": "Send TTS Messages", "speak": "Speak", "use_voice_activation": "No Voice Activity",
+                      "view_audit_log": "View the Server Audit Log"}
 
         order = [1, 28, 13, 16, 11, 10, 3, 6, 4, 15, 12, 17, 23, 24, 25, 14, 8, 2, 22, 18, 9, 0, 5, 26, 20, 7, 19, 27, 21]
         permtuples = []
