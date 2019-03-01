53b2fbe0a33c2c0f25a2fab4bf8c3d2f7c41c60d
Dante Dam <laggycomputer@yahoo.com>
2019-03-01 08:09:52 -0800

    Help command revamp

--- suprkewl-bot/ext/help.py
+++ suprkewl-bot/ext/help.py
@@ -5,29 +5,57 @@ class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(description="Please note that if you specify a cog name that is also the name of a command, the help for the command will be given.")
     async def help(self, ctx, specific=None):
         """Shows this message."""
 
         if specific:
+
+            command = self.bot.get_cog(speific)
             command = self.bot.get_command(specific)
 
             if command is None:
-                await ctx.send(":x: Command not found!")
+                await ctx.send(":x: Command/cog not found!")
                 return
             else:
-                emb = discord.Embed(title=f"{ctx.prefix}{command.qualified_name}{', ' if any(command.aliases) else ''}{', '.join(command.aliases)}", description=command.short_doc, color=0xf92f2f)
+                if isinstance(command, commands.Cog):
+                    cog = command
+                    del command
 
-                if isinstance(command, commands.GroupMixin):
-                    for subcommand in command.commands:
-                        emb.add_field(name=f"`{ctx.prefix}{subcommand.qualified_name}{', ' if any(subcommand.aliases) else ''}{', '.join(subcommand.aliases)}`", value=subcommand.short_doc + "\u200b")
+                    emb = discord.Embed(title=f"{ctx.prefix}{cog.name}")
 
+                    for command in cog.walk_commands():
+                        alist = []
+                        for alias in command.aliases:
+                           alist.append(f"`{ctx.prefix}{alias}`")
+
+                        emb.add_field(name=f"`{ctx.prefix}{command.qualified_name}`{', ' if any(command.aliases) else ''}{', '.join(alist)}", description=command.short_doc + "\u200b")
+                else:
+                    alist = []
+                    for alias in command.aliases:
+                        alist.append(f"`{alias}`")
+
+                    emb = discord.Embed(title=f"{ctx.prefix}{command.qualified_name}{', ' if any(command.aliases) else ''}{', '.join(alist))}", description=command.short_doc, color=0xf92f2f)
+                    emb.add_field(name="Full description", value=command.description + "\u200b")
+
+                    if isinstance(command, commands.GroupMixin):
+                        for subcommand in command.commands:
+                            alist = []
+                            for alias in subcommand.aliases:
+                                alist.append(f"`{ctx.prefix}{alias}`")
+
+                            emb.add_field(name=f"`{ctx.prefix}{subcommand.qualified_name}`{', ' if any(subcommand.aliases) else ''}{', '.join(alist)}", value=subcommand.short_doc + "\u200b")
         else:
             emb = discord.Embed(title="Help and Information", color=0xf92f2f)
             for name, command in self.bot.all_commands.items():
                 if name in command.aliases or (command.hidden and not await self.bot.is_owner(ctx.author)):
                     continue
-                emb.add_field(name=f"{name}{', ' if any(command.aliases) else ''}{', '.join(command.aliases)}", value=command.short_doc + "\u200b")
+
+                alist = []
+                for alias in command.aliases:
+                    alist.append(f"`{ctx.prefix}{alias}`")
+
+                emb.add_field(name=f"{name}{', ' if any(command.aliases) else ''}{', '.join(alist)}", value=command.short_doc + "\u200b")
 
         emb.set_thumbnail(url=self.bot.user.avatar_url)
         emb.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
