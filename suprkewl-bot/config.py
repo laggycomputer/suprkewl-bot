0e05943f6650cff6c55c011fedeb7c7bdb6d4a3a
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2019-03-03 09:51:44 -0800

    Added config file.

---
+++ suprkewl-bot/config.py
@@ -0,0 +1,10 @@
+import logging
+
+# Your Discord bot token here:
+token = ""
+# Logging path and level.
+logpath = "../suprkewl.log"
+loglevels = [logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
+# You can set this to one of the above values, or try your own integer value from 0 to 50 inclusive.
+# See the logging docs at https://docs.python.org/3/library/logging.html#levels for more info.
+loglevel = loglevels[4]
