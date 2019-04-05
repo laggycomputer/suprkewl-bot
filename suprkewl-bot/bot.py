5d680b7a508d64e1353a2910664909beed4aa1cc
Dante Dam <laggycomputer@yahoo.com>
2019-04-04 21:07:10 -0700

    Added admin-only cog and command to delete a message.

--- suprkewl-bot/bot.py
+++ suprkewl-bot/bot.py
@@ -46,7 +46,7 @@ class theBot(commands.Bot):
         self.bg_task = self.loop.create_task(self.playingstatus())
 
         startup_extensions = [
-            "jishaku", "ext.text", "ext.rand", "ext.docs", "ext.mod", "ext.info", "ext.help", "ext.config"
+            "jishaku", "ext.text", "ext.rand", "ext.docs", "ext.mod", "ext.info", "ext.help", "ext.config", "ext.admin"
         ]
 
         for extension in startup_extensions:
