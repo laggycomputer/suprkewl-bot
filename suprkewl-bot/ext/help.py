923792f87839e2db6bbd583da66fa7c360b2c3e8
Dante Dam <laggycomputer@yahoo.com>
2019-04-30 19:19:51 -0700

    Bot help fix

--- suprkewl-bot/ext/help.py
+++ suprkewl-bot/ext/help.py
@@ -152,7 +152,7 @@ class HelpCommand(commands.HelpCommand):
 
     def add_command_field(self, embedinator, command):
         name = self.get_command_name(command)
-        embedinator.add_field(name=name, value=command.short_doc, inline=False)
+        embedinator.add_field(name=name, value=command.short_doc or "\u200b", inline=False)
 
     def get_opening_note(self):
         command_name = self.context.invoked_with
