7a281f4d2240836101b0227bb773e4f1aea9fef3
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2019-05-01 20:19:34 -0700

    Added R Danny-like commit list.

--- suprkewl-bot/ext/docs.py
+++ suprkewl-bot/ext/docs.py
@@ -20,11 +20,14 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-import subprocess
+import datetime
+import itertools
 
 import discord
 from discord.ext import commands
+import pygit2
 
+from .utils import time
 
 class Documentation(commands.Cog):
 
@@ -43,21 +46,28 @@ class Documentation(commands.Cog):
         sent = (await ctx.send(embed=emb))
         await ctx.bot.register_response(sent, ctx.message)
 
+    # Largely from R. Danny.
+    def format_commit(self, commit):
+        short, _, _ = commit.message.partition('\n')
+        short_sha2 = commit.hex[0:6]
+        commit_tz = datetime.timezone(datetime.timedelta(minutes=commit.commit_time_offset))
+        commit_time = datetime.datetime.fromtimestamp(commit.commit_time).replace(tzinfo=commit_tz)
+
+        offset = time.human_timedelta(commit_time.astimezone(datetime.timezone.utc).replace(tzinfo=None), accuracy=1)
+        return f"[`{short_sha2}`](https://github.com/laggycomputer/suprkewl-bot/commit/{commit.hex}) {short} ({offset})"
+
+    def _get_last_commits(self, count=3):
+        repo = pygit2.Repository(".git")
+        commits = list(itertools.islice(repo.walk(repo.head.target, pygit2.GIT_SORT_TOPOLOGICAL), count))
+        return "\n".join(self.format_commit(c) for c in commits)
+
     @commands.command(aliases=["git", "source"])
     async def github(self, ctx):
         """A link to our GitHub repo."""
 
-        emb = discord.Embed(name="Our GitHub", color=0xf92f2f)
+        emb = discord.Embed(name="Our GitHub", color=0xf92f2f, description=self._get_last_commits())
         emb.add_field(name="Our github", value="https://www.github.com/laggycomputer/suprkewl-bot")
         emb.add_field(name="Clone it!", value="`git clone https://github.com/laggycomputer/suprkewl-bot.git`")
-
-        head_hash = subprocess.run(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE).stdout
-        head_hash = head_hash.decode().rstrip("\n")
-        emb.add_field(
-            inline=False,
-            name="Current HEAD",
-            value=f"[Here](https://github.com/laggycomputer/suprkewl-bot/commit/{head_hash})"
-        )
 
         emb.set_thumbnail(url=ctx.bot.user.avatar_url)
         emb.set_author(name=ctx.bot.user.name, icon_url=ctx.bot.user.avatar_url)
--- suprkewl-bot/ext/utils/__init__.py
+++ suprkewl-bot/ext/utils/__init__.py
@@ -2,4 +2,5 @@ from .apiToHuman import apiToHuman
 from .codeblock import escape_codeblocks
 from .embedinator import Embedinator
 from .jsonfmt import format_json
-from .plural import plural
+from .plural import plural, Plural
+from .time import human_timedelta
