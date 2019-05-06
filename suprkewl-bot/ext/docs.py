6d44684f6f94d1d222abd5f50c5da288bdaa36d5
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2019-05-06 11:07:46 -0700

    added linecount command

--- suprkewl-bot/ext/docs.py
+++ suprkewl-bot/ext/docs.py
@@ -20,8 +20,11 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
+import codecs
 import datetime
 import itertools
+import os
+import pathlib
 
 import discord
 from discord.ext import commands
@@ -81,6 +84,26 @@ class Documentation(commands.Cog):
         """A support server link."""
 
         sent = (await ctx.send("https://discord.gg/CRBBJVY"))
+        await ctx.bot.register_response(sent, ctx.message)
+
+    @commands.command(aliases=["wc", "codesize"])
+    async def linecount(self, ctx):
+        path_to_search = "./"
+        total = 0
+        file_amount = 0
+        for path, subdirs, files in os.walk(path_to_search):
+            for name in files:
+                    if name.endswith(".py"):
+                        file_amount += 1
+                        with codecs.open(path_to_search + str(pathlib.PurePath(path, name)), "r", "utf-8") as f:
+                            for i, l in enumerate(f):
+                                if l.strip().startswith("#") or len(l.strip()) is 0:
+                                    pass
+                                else:
+                                    total += 1
+
+        msg = f"I am made of {total:,} lines of Python, spread across {file_amount:,} files!"
+        sent = (await ctx.send(msg))
         await ctx.bot.register_response(sent, ctx.message)
 
 def setup(bot):
