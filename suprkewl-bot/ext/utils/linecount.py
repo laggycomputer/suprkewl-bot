68ecafe126e7588098759fca406d0370634b14c7
Dante Dam <37320033+laggycomputer@users.noreply.github.com>
2019-05-09 21:25:36 -0700

    Added randomized embed footer / bot description.

---
+++ suprkewl-bot/ext/utils/linecount.py
@@ -0,0 +1,21 @@
+import codecs
+import os
+import pathlib
+
+
+def linecount():
+    path_to_search = "./"
+    total = 0
+    file_amount = 0
+    for path, subdirs, files in os.walk(path_to_search):
+        for name in files:
+            if name.endswith(".py"):
+                file_amount += 1
+                with codecs.open(path_to_search + str(pathlib.PurePath(path, name)), "r", "utf-8") as f:
+                    for i, l in enumerate(f):
+                        if l.strip().startswith("#") or len(l.strip()) is 0:
+                            pass
+                        else:
+                            total += 1
+
+    return f"I am made of {total:,} lines of Python, spread across {file_amount:,} files!"
