aeb33a3a5cd7e37a3f7189ddec97afe30a020f1c
Dante Dam <laggycomputer@yahoo.com>
2019-05-09 20:49:43 -0700

    Moved raw command to info cog

--- suprkewl-bot/ext/text.py
+++ suprkewl-bot/ext/text.py
@@ -20,77 +20,12 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import io
 import os
 import random
-import typing
 
 import discord
 from discord.ext import commands
 
-from .utils import escape_codeblocks, format_json
-
 
 class Text(commands.Cog):
-
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.group(
-        description="Gives the data under a message, channel, or member in a JSON format, as recieved from the Discord API."
-    )
-    async def raw(self, ctx):
-        """Returns a dict version of some objects."""
-
-        if ctx.invoked_subcommand is None:
-            sent = (await ctx.send(":x: Please give a subcommand!"))
-            await ctx.bot.register_response(sent, ctx.message)
-
-    @raw.command(name="message", aliases=["msg"])
-    async def raw_message(self, ctx, message: discord.Message):
-        """Return a message as a dict."""
-
-        raw = await ctx.bot.http.get_message(message.channel.id, message.id)
-
-        try:
-            sent = (await ctx.send(f"```json\n{escape_codeblocks(format_json(raw))}```"))
-            await ctx.bot.register_response(sent, ctx.message)
-        except discord.HTTPException:
-            raw_string = "```json\n{}```".format(escape_codeblocks(format_json(raw)))
-            half = int(len(raw_string) / 2)
-            raw_string = [raw_string[0:half] + "```", "```json\n" + raw_string[half:len(raw_string)]]
-            await ctx.send(raw_string[0])
-            await ctx.send(raw_string[1])
-
-    @raw.command(name="member", aliases=["user"])
-    async def raw_member(self, ctx, user: discord.User = None):
-        """Return a member as a dict."""
-        if user is None:
-            user = ctx.author
-
-        route = discord.http.Route("GET", f"/users/{user.id}")
-        raw = await ctx.bot.http.request(route)
-
-        sent = (await ctx.send(f"```json\n{escape_codeblocks(format_json(raw))}```"))
-        await ctx.bot.register_response(sent, ctx.message)
-
-    @raw.command(name="channel")
-    async def raw_channel(
-            self, ctx, channel: typing.Union[
-                discord.TextChannel, discord.VoiceChannel, discord.CategoryChannel
-            ]=None
-    ):
-        """Return a channel as a dict."""
-
-        if channel is None:
-            channel = ctx.channel
-
-        route = discord.http.Route("GET", f"/channels/{channel.id}")
-        try:
-            raw = await ctx.bot.http.request(route)
-        except discord.Forbidden:
-            sent = (await ctx.send(":x: I can't see info on that channel!"))
-            await ctx.bot.register_response(sent, ctx.message)
-
-            return
-
-        sent = (await ctx.send(f"```json\n{escape_codeblocks(format_json(raw))}```"))
-        await ctx.bot.register_response(sent, ctx.message)
 
     @commands.command(description="Sends text. Strict cooldown.")
     @commands.cooldown(1, 120, commands.BucketType.channel)
