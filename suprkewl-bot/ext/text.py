9b13a7ba0bd68f475eda046b97f7b7da6d0fd2d8
Cheeseinator <50121172+Cheeseinator@users.noreply.github.com>
2019-05-20 18:46:53 -0500

    Added lettuce.txt for s!text, see #2

--- suprkewl-bot/ext/text.py
+++ suprkewl-bot/ext/text.py
@@ -32,7 +32,7 @@ class Text(commands.Cog):
     async def text(self, ctx):
         """Don't ask what this does."""
 
-        files = ["bee.txt", "uwu.txt"]
+        files = ["bee.txt", "lettuce.txt", "uwu.txt"]
         with open(os.getcwd() + f"..\\..\\assets\\{random.choice(files)}", "rb") as fp:
             sent = (await ctx.send(file=discord.File(fp, filename="love_letter.txt")))
         await ctx.bot.register_response(sent, ctx.message)
