7faba33faac039d612c7ed1f221fbc2f62ec139a
Dante Dam <laggycomputer@yahoo.com>
2019-06-13 20:51:41 -0700

    Use wildcard imports for utils __init__

--- suprkewl-bot/ext/utils/__init__.py
+++ suprkewl-bot/ext/utils/__init__.py
@@ -1,10 +1,10 @@
 # Sorry about the ignore tactics, this really is my way of doing it.
 
-from .codeblock import escape_codeblocks  # noqa
-from .context import Context  # noqa
-from .embedinator import Embedinator  # noqa
-from .jsonfmt import format_json  # noqa
-from .linecount import linecount  # noqa
-from .permissions_converter import permissions_converter  # noqa
-from .plural import plural, Plural  # noqa
-from .time import human_timedelta  # noqa
+from .codeblock import *  # noqa
+from .context import *  # noqa
+from .embedinator import *  # noqa
+from .jsonfmt import *  # noqa
+from .linecount import *  # noqa
+from .permissions_converter import *  # noqa
+from .plural import *  # noqa
+from .time import *  # noqa
