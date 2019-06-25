5d99654a0966d6a6f65861881842a2b217a68ff4
Dante Dam <laggycomputer@yahoo.com>
2019-06-24 19:00:21 -0700

    Add 8ball command
    you can tell i'm out of ideas

--- suprkewl-bot/ext/fun.py
+++ suprkewl-bot/ext/fun.py
@@ -867,6 +867,18 @@ L
 
         await ctx.send(file=discord.File(io.BytesIO(raw), "supreme.png"))
 
+    @commands.command(name="8ball", aliases=["8b"])
+    async def _8ball(self, ctx, *, question):
+        """The classic Magic 8 ball."""
+
+        await ctx.send(random.choice([
+            "It is certain.", "Without a doubt.", "You may rely on it.", "Yes, definitely.", "It is decidedly so.",
+            "As I see it, yes.", "Most likely.", "Yes.", "Outlook good.", "Signs point to yes.",
+            "Reply hazy try again.", "Better not tell you now.", "Ask again later.", "Cannot predict now.",
+            "Concentrate and ask again.", "Don’t count on it.", "Outlook not so good.", "My sources say no.",
+            "Very doubtful.", "My reply is no."
+        ]))
+
 
 def setup(bot):
     bot.add_cog(Fun())
