19b8cf297df577f8c8fec257c3bad89faff61b5d
Dante Dam <laggycomputer@yahoo.com>
2019-07-03 15:41:39 -0700

    Remove unused util

--- suprkewl-bot/ext/utils/format_and_convert.py
+++ suprkewl-bot/ext/utils/format_and_convert.py
@@ -63,19 +63,6 @@ def format_json(string):
     return json.dumps(string, indent=2, ensure_ascii=False, sort_keys=True)
 
 
-def plural(values):
-    if len(values) == 0:
-        return ""
-    elif len(values) == 1:
-        return values[0]
-    elif len(values) == 2:
-        return f"{values[0] and values[1]}"
-    else:
-        ret = ", ".join(values[:-3])
-        ret += f", {values[-2]}, and {values[-1]}"
-        return ret
-
-
 class Plural:  # From R. Danny.
 
     def __init__(self, value):
