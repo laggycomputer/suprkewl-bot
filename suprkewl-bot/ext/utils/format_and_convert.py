c575a911b4b43fd2297594f267d43bae54bc3ea4
Dante Dam <laggycomputer@yahoo.com>
2019-07-13 08:21:59 -0700

    code format tweaks

--- suprkewl-bot/ext/utils/format_and_convert.py
+++ suprkewl-bot/ext/utils/format_and_convert.py
@@ -45,16 +45,16 @@ def escape_codeblocks(line):
     i = 0
     n = 0
     while i < len(line):
-        if (line[i]) == "`":
+        if line[i] == "`":
             n += 1
         if n == 3:
-            line = line[:i] + '\u200b' + line[i:]
+            line = line[:i] + "\u200b" + line[i:]
             n = 1
             i += 1
         i += 1
 
     if line[-1] == "`":
-        line += '\u200b'
+        line += "\u200b"
 
     return line
 
