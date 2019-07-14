eb79aa91ae845fc6568d4dd4a9c563a6ff2944f1
Dante Dam <laggycomputer@yahoo.com>
2019-07-14 10:17:50 -0700

    Add an embed footer

--- suprkewl-bot/bot.py
+++ suprkewl-bot/bot.py
@@ -398,7 +398,8 @@ class suprkewl_bot(commands.Bot):
             " even number?",
             "Man cannot live by bread alone; he must have peanut butter.",
             "May all your bacon burn.",
-            "Regex by trial and error: Combining dots slashes and dots until a thing happens.",
+            "Regex by trial and error: Combining dots slashes and dots until a thing happens."
+            "The center of a donut is 100% fat free.",
             "The word utopia is derived from the Latin 'Utopia', meaning 'nothing, impossible'.",
             "To succeed in life, you need three things; a wishbone, a backbone and a funny bone. - Reba McEntire",
             linecount(),
