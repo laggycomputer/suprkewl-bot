139711f25eb6b1655e2a006ca938e92c19e81c12
Dante Dam <laggycomputer@yahoo.com>
2020-03-30 16:31:18 -0700

    it's march, i PRed d.py to bump their license year, i haven't done mine

--- suprkewl-bot/redis.py
+++ suprkewl-bot/redis.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2019 laggycomputer
+Copyright (C) 2020 Dante "laggycomputer" Dam
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
