e01cf6b5dcfba9bad8d5dad1cb1e42b7f8bdfaed
Dante Dam <laggycomputer@yahoo.com>
2020-11-16 11:02:33 -0800

    Yeet legacy meme code

--- suprkewl-bot/bot.py
+++ suprkewl-bot/bot.py
@@ -36,9 +36,6 @@ import redis
 
 
 MOYAI_GUILD_ID = 679073831629094922
-MOYAI_CHANNEL_ID = 679073833717596300
-ALFALFA_GUILD_ID = 555087033652215830
-ALFALFA_CHANNEL_ID = 694313862295715870
 
 
 class suprkewl_bot(commands.Bot):
@@ -152,27 +149,8 @@ class suprkewl_bot(commands.Bot):
 
             await self.track_message(f"tracked_message {message.id}")
 
-            if isinstance(message.channel, discord.abc.GuildChannel):
-                if message.guild.id == MOYAI_GUILD_ID and message.channel.id == MOYAI_CHANNEL_ID:
-                    if not await self.is_blacklisted(message.author.id):
-                        if message.channel.permissions_for(message.guild.me).add_reactions:
-                            await message.add_reaction(":moyai:")
-                            if message.content:
-                                demoyaified = message.content.replace(":moyai:", "").replace("moyai", "")
-                                if not demoyaified or demoyaified.isspace():
-                                    await message.add_reaction(":regional_indicator_m:")
-                                    await message.add_reaction(":regional_indicator_o:")
-                                    await message.add_reaction(":regional_indicator_y:")
-                                    await message.add_reaction(":regional_indicator_a:")
-                                    await message.add_reaction(":regional_indicator_i:")
-
+            if message.guild:
                 if message.channel.permissions_for(message.guild.me).send_messages:
-                    if message.guild.id == ALFALFA_GUILD_ID and message.channel.id == ALFALFA_CHANNEL_ID:
-                        if not await self.is_blacklisted(message.author.id):
-                            if await self.is_owner(message.author) and message.content.endswith("lfa"):
-                                if len(message.content) + 3 <= 2000:
-                                    await message.channel.send(message.content + "lfa")
-
                     if message.guild.me in message.mentions:
                         ping_images = ("angery,gif", "eyes.png")
 
