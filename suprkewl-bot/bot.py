e59b7c737550f2596963a7d3314b7bc7002d6138
Dante Dam <laggycomputer@yahoo.com>
2021-09-21 14:52:18 -0700

    typo fix

--- suprkewl-bot/bot.py
+++ suprkewl-bot/bot.py
@@ -457,7 +457,7 @@ class SuprKewlBot(commands.Bot):
             emb = ctx.default_embed()
             emb.add_field(
                 name="Blacklisted command",
-                value=f":x: `{ctx.prefix}{ctx.command}` has bee disabled in this context. Have another user try again, "
+                value=f":x: `{ctx.prefix}{ctx.command}` has been disabled in this context. Have another user try again, "
                       f"or try again in another channel, server, or DM."
             )
 
